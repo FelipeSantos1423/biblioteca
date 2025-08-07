fab79ce88962e93adca5db999aab410d902ff3a1
Felipe <felipsantonios@gmail.com>
2025-08-06 22:47:44 -0300

    arrumando nome da página

--- public/exibe-dados.php
+++ public/exibe-dados.php
@@ -19,7 +19,7 @@ $livros = $livroRepo->listarTodos(); // Exibe todos os livros, como você queria
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Conta | Dashboard</title>
+    <title>Biblioteca Digital</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
